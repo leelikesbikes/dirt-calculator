29e9c4f27c97a0b4f81fd2257ac9a86808dc13b0
leelikesbikes <leelikesbikes@gmail.com>
2026-04-27 12:20:42 -0600

    Update password-config.js
    updating password from Dialed2Eleven to DialedTwo11

--- app/password-config.js
+++ app/password-config.js
@@ -1,7 +1,7 @@
 // DiRT Calculator Password Protection
 // To change the password, update this line and redeploy:
 
-export const DIRT_PASSWORD = "Dialed2Eleven";
+export const DIRT_PASSWORD = "DialedTwo11";
 
 // Password is case-sensitive
 // Keep this file secure - don't share publicly
